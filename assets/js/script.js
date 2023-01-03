0be71d2b6dd6a554e505e6a1d414be950e36ed66
Tonywilson1211 <ant.wilson@hotmail.com>
2023-01-03 13:36:10 +0000

    Add integer value to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,7 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2); 
     } else if (gameType === "division") {
-        displayDevisionQuestion(num1, num2);
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,12 +89,13 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 / operand2, "divison"];
+            return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
+
 
 
 /**
@@ -132,7 +133,8 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDevisionQuestion(operand1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
